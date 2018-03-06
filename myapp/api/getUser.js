1d9f6eeaef4215039e5ed701d5a87aef18a1b01a
陈隆 <772319837@qq.com>
2018-03-07 00:02:57 +0800

    传递到服务器上面 然后修改大小写到问题

--- myapp/api/getUser.js
+++ myapp/api/getUser.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var dbConfig = require('../db/DBConfig');
-var UserSQL = require('../db/Sql');
+var UserSQL = require('../db/SQL');
 var pool = mysql.createPool(dbConfig.mysql);
 
 
